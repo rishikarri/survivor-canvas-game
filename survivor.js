983820ed20b87a9355643707192a9ec48966ec96
Rishi Karri <rishikarri1@gmail.com>
2016-12-08 18:41:32 -0500

    Added an arrow to the screen

--- survivor.js
+++ survivor.js
@@ -35,10 +35,27 @@ var goblinLocation = {
 	destinationY: 400
 }
 
+//create arrow object
+var arrow = new Image(); 
+arrow.src = "Images/arrow-right.png";
+var arrowLocation = {
+	x: (robinHoodLocation.x + 20),
+	y: (robinHoodLocation.y + 4),
+	destinationX: (robinHoodLocation.x + 20),
+	destinationY: (robinHoodLocation.y + 4)
+	
+}
+
+// HERO STAT Variables 
+var healthPoints = 20; 
+
 
 function update(){
 	moveRobinHood();
 	moveGoblin();
+	collisionDetection();
+	shoot();
+	checkGameStatus();
 
 }
 //MOVING SECTION 
@@ -61,61 +78,105 @@ function moveRobinHood(){
 	if(37 in keysPressed){
 		if (robinHoodLocation.x >= 32){
 			robinHoodLocation.x -= 7 * speedModifier;
+			arrowLocation.x -= 7 * speedModifier;
 		}
 	}
 	if (38 in keysPressed){
 		if (robinHoodLocation.y >= 30){
 			robinHoodLocation.y -= 7 * speedModifier;
+			arrowLocation.y -= 7 * speedModifier;
 		}
 	}
 	if (39 in keysPressed){
 		if (robinHoodLocation.x <= 440){
 			robinHoodLocation.x += 7 * speedModifier;
+			arrowLocation.x += 7 * speedModifier;
 		}
 	}
 	if (40 in keysPressed){
 		if (robinHoodLocation.y <= 405){
 			robinHoodLocation.y += 7 * speedModifier;
+			arrowLocation.y += 7 * speedModifier;
 		}
 	}
 }
 
-//create goblin destinations 
+//create goblin movement 
 
-
+var goblinSpeedModifier = 1;
 function moveGoblin(){
 	//if he is already at his destination, generate a random one, otherwise move to destination
-	if(
-		(Math.abs(goblinLocation.x - goblinLocation.destinationX)) < 32 
-		&& Math.abs(goblinLocation.y - goblinLocation.destinationY) < 32
-	){
-		goblinLocation.destinationX = Math.random() * 650; 
-		goblinLocation.destinationY = Math.random() * 650; 
-		// show me the new destination when teh goblin reaches the one it's trying to get to
-		console.log(goblinLocation.x, goblinLocation.y)
-		console.log(goblinLocation.destinationX, goblinLocation.destinationY);
-		
+	if (Math.abs(goblinLocation.x - goblinLocation.destinationX) < 32) {
+		goblinLocation.destinationX = Math.random() * 440 + 40; 
 	}else if(goblinLocation.x < goblinLocation.destinationX){
-		goblinLocation.x += 4; 
-	}else if(goblinLocation.x > goblinLocation.destinationX){
-		goblinLocation.x -= 4; 
-	}else if(goblinLocation.y > goblinLocation.destinationY){
-		goblinLocation.y -= 4; 
-	}else if(goblinLocation.y < goblinLocation.destinationY){
-		goblinLocation.y +=4;
-	}//start here and check to see if goblin is moving properly
-	console.log(goblinLocation.x, goblinLocation.destinationX);
+		goblinLocation.x += 3 * goblinSpeedModifier;
+		// console.log(monsterNewDestinationX, monsterLocation.x);
+	}else{
+		goblinLocation.x -= 3 * goblinSpeedModifier;
+	}
 	
-};
+	if (Math.abs(goblinLocation.y - goblinLocation.destinationY) < 32) {
+		goblinLocation.destinationY = Math.random() * 400 + 20; 
+	}else if(goblinLocation.y > goblinLocation.destinationY){
+		goblinLocation.y -= 3 * goblinSpeedModifier;
+		// console.log(monsterNewDestinationY, monsterLocation.y);
 
+	}else{
+		goblinLocation.y += 3 * goblinSpeedModifier;
+	}
+	// console.log(goblinLocation.x, goblinLocation.y);
+	// console.log(goblinLocation.destinationX, goblinLocation.destinationY);
+}	
+
+
+// collision detection section
+function collisionDetection(){
+
+	// if the goblin catches robinhood then lower his health
+	if(
+		Math.abs((goblinLocation.x - robinHoodLocation.x)) < 32
+		&& Math.abs(goblinLocation.y - robinHoodLocation.y) < 32
+	){
+		//robin hoood got hit
+		goblinLocation.x = Math.random() * 440 + 40; 
+		goblinLocation.y = Math.random() * 400 + 20; 
+		healthPoints--;
+		document.getElementById("health").innerHTML = "Health: " + healthPoints; 
+		
+	}
+}
+
+function shoot(){
+	// if the spacebar is hit, shoot the arrow 100 pixels right
+	if(32 in keysPressed){
+		arrowLocation.destinationX = arrowLocation.x + 100; 
+		arrowLocation.destinationY = arrowLocation.y; 
+		console.log(arrowLocation.x, arrowLocation.destinationX)
+	}
+	
+
+}
+
+//Game status section
+var gameOn = true;
+function checkGameStatus(){
+	if(healthPoints <= 0){
+		gameOn = false;
+		document.getElementById("textDisplay").innerHTML = "GAME OVER!"
+	}
+}
 
 //need to draw the image constantly
 
 function draw() {
-	update();
+	if (gameOn){
+		update();
+	}
+	
 	context.drawImage(backgroundImage, 0, 0);
 	context.drawImage(robinHood, robinHoodLocation.x, robinHoodLocation.y);
 	context.drawImage(goblin, goblinLocation.x, goblinLocation.y);
+	context.drawImage(arrow, arrowLocation.x, arrowLocation.y);
 	requestAnimationFrame(draw);
 };
 
